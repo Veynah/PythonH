ce678ee07a8c62d9d7611831225b6641dce74caa
AdamTangiev <tangiev96@gmail.com>
2023-10-27 23:23:56 +0200

    adding some code + venv for linux
    Need to finish netcat

---
+++ bhp/code/netcat.py
@@ -0,0 +1,23 @@
+import argparse
+import socket
+import shlex
+import subprocess
+import sys
+import textwrap
+import threading
+
+def execute(cmd):
+    cmd = cmd.strip()
+    if not cmd:
+        return
+    output = subprocess.check_output(shlex.split(cmd)), stderr = subprocess.STDOUT)
+    return output.decode()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='BHP Net Tool',
+        formatter_class=argparse.RawDescriptionHelpFormetter,
+        epilog=textwrap.dedent('''
+            
+        ''')
+    )
